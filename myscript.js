7fa4a8ebb30fae9e81c09c365201b4d56d31be1d
BenjaminSemah <88531553+BenjaminSemah@users.noreply.github.com>
2021-11-15 15:21:27 +0000

    add computerPlay and userPlay functions
    I have created 2 functions for the game. One takes the computer's input and the other takes the input from the user.

--- myscript.js
+++ myscript.js
@@ -1,1 +1,23 @@
+//Function to randomly select one of three options
+function computerPlay() {
+    let compArray = ["ROCK", "PAPER", "SCISSORS"];
+    let compSelect = compArray[Math.floor(Math.random()*compArray.length)];
+    return compSelect;
+}
+computerPlay();
 
+function userPlay() {
+    let user = prompt("Rock Paper Scissors ???");
+    let userInput = user.toUpperCase();
+    if (userInput === "ROCK") {
+        console.log(userInput);
+    }
+    else if (userInput === "PAPER") {
+        console.log(userInput);
+    }
+    else if(userInput === "SCISSORS") {
+        console.log(userInput);
+    } 
+    else(console.log("Wrong Input! Try again."))
+}
+userPlay();
